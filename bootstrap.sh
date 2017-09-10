22e3dfd393652930b14083991541925792b855a3
jpvg95 <juan.valencia10a@gmail.com>
2017-09-10 00:37:34 -0500

    Added MongoDB

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,15 @@
-apt-get update
+# Update packaged
+sudo apt-get update
 
+# Install Node.js and NPM
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 
+# Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+# Start the Mongo service
+sudo service mongod start
--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/', function(error){
+	if(error) console.log(error);
+});
+
 app.get('/', function(req, res){
-	res.send('Hello Vagrant');
+	var data = '<p>Hello Vagrant!</p>';
+	if(mongoose.connection.readyState){
+		data += '<p>Connected to MongoDB</p>';
+	}else{
+		data += '<p>Not connected to MongoDB :(</p>';
+	}
+	res.send(data);
 });
 
 app.listen('3000');
