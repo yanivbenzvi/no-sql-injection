551a74a668fbee7884e3a3f45ce843bde2821091
jpvg10 <juan.valencia10a@gmail.com>
2019-05-26 13:48:17 -0500

    Changes in server code

--- src/server.js
+++ src/server.js
@@ -3,19 +3,27 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/', function(error){
-	if(error) console.log(error);
+mongoose.connect('mongodb://127.0.0.1/myapp', { useNewUrlParser: true });
+
+mongoose.connection.on('connected', () => {
+  console.log('Database connected');
 });
 
-app.get('/', function(req, res){
-	var data = '<p>Hello Vagrant!</p>';
-	if(mongoose.connection.readyState){
-		data += '<p>Connected to MongoDB</p>';
-	}else{
-		data += '<p>Not connected to MongoDB :(</p>';
-	}
-	res.send(data);
+mongoose.connection.on('error', error => {
+  console.log('Database connection failed');
+  console.log(error);
 });
 
-app.listen('3000');
-console.log('Listening on port 3000');
+app.get('/', (req, res) => {
+  var data = '<p>Hello Vagrant!</p>';
+  if (mongoose.connection.readyState) {
+    data += '<p>Connected to MongoDB</p>';
+  } else {
+    data += '<p>Not connected to MongoDB :(</p>';
+  }
+  res.send(data);
+});
+
+app.listen('3000', () => {
+  console.log('Listening on port 3000');
+});
