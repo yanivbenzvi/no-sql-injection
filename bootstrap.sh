5844e478a8365ed3768061dca866bd7f6b846ef7
jpvg10 <juan.valencia10a@gmail.com>
2019-05-26 12:35:49 -0500

    Updating ubuntu, node and mongo

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,16 @@
 sudo apt-get update
 
 # Install Node.js and NPM
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get install -y npm
+
+# Install npm@5.7.1
+# Temporary fix for issue: https://github.com/npm/npm/issues/20605
+sudo npm install -g npm@5.7.1
 
 # Install MongoDB
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
